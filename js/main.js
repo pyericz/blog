041101659446003f479e8a2d1d68f1bcc224715d
Eric Zhang <eric.zhang@avatarworks.com>
2017-08-24 19:15:32 +0800

    touch screen touch behavior

--- js/main.js
+++ js/main.js
@@ -9,33 +9,37 @@
  */
 (function() {
     if (window.innerWidth <= 770) {
-        var menuBtn = document.querySelector('#headerMenu')
-        var nav = document.querySelector('#headerNav')
+        var menuBtn = document.querySelector('#headerMenu');
+        var nav = document.querySelector('#headerNav');
         menuBtn.onclick = function(e) {
-            e.stopPropagation()
+            e.stopPropagation();
             if (menuBtn.classList.contains('active')) {
-                menuBtn.classList.remove('active')
-                nav.classList.remove('nav-show')
+                menuBtn.classList.remove('active');
+                nav.classList.remove('nav-show');
             } else {
-                nav.classList.add('nav-show')
-                menuBtn.classList.add('active')
+                nav.classList.add('nav-show');
+                menuBtn.classList.add('active');
             }
-        }
-        document.querySelector('body').addEventListener('click', function() {
-            nav.classList.remove('nav-show')
-            menuBtn.classList.remove('active')
-        })
+        };
 
+        document.querySelector('body').addEventListener('click', function() {
+            nav.classList.remove('nav-show');
+            menuBtn.classList.remove('active');
+        });
+        document.querySelector('body').addEventListener('touchend', function() {
+            nav.classList.remove('nav-show');
+            menuBtn.classList.remove('active');
+        });
     }
 }());
 
 
 //////////////////////////back to top////////////////////////////
 (function() {
-    var backToTop = document.querySelector('.back-to-top')
-    var backToTopA = document.querySelector('.back-to-top a')
-    var menuBtn = document.querySelector('#headerMenu')
-    var nav = document.querySelector('#headerNav')
+    var backToTop = document.querySelector('.back-to-top');
+    var backToTopA = document.querySelector('.back-to-top a');
+    var menuBtn = document.querySelector('#headerMenu');
+    var nav = document.querySelector('#headerNav');
         // console.log(backToTop);
     window.addEventListener('scroll',function () {
         // if (menuBtn.classList.contains('active')) {
@@ -44,23 +48,23 @@
         // }
 
         // 页面顶部滚进去的距离
-        var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
+        var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
 
         if (scrollTop > 200) {
-            backToTop.classList.add('back-to-top-show')
+            backToTop.classList.add('back-to-top-show');
         } else {
-            backToTop.classList.remove('back-to-top-show')
+            backToTop.classList.remove('back-to-top-show');
         }
-    })
+    });
     // $("a[href='#top']").click(function() {
     //     $("html, body").animate({ scrollTop: 0 }, "slow");
     //     return false;
     // });
     backToTopA.addEventListener('click',function (e) {
-        e.preventDefault()
+        e.preventDefault();
         // window.scrollTo(0,0)
         $("html, body").animate({ scrollTop: 0 }, "fast");
-    })
+    });
 }());
 
 (function() {
