0234d99f3938c0d3242c7d519c4ba8485c6922ee
Eric Zhang <eric.zhang@avatarworks.com>
2017-08-25 23:00:49 +0800

    test

--- js/main.js
+++ js/main.js
@@ -11,16 +11,16 @@
     if (window.innerWidth <= 770) {
         var menuBtn = document.querySelector('#headerMenu');
         var nav = document.querySelector('#headerNav');
-        menuBtn.onclick = function(e) {
-            e.stopPropagation();
-            if (menuBtn.classList.contains('active')) {
-                menuBtn.classList.remove('active');
-                nav.classList.remove('nav-show');
-            } else {
-                nav.classList.add('nav-show');
-                menuBtn.classList.add('active');
-            }
-        };
+        // menuBtn.onclick = function(e) {
+        //     e.stopPropagation();
+        //     if (menuBtn.classList.contains('active')) {
+        //         menuBtn.classList.remove('active');
+        //         nav.classList.remove('nav-show');
+        //     } else {
+        //         nav.classList.add('nav-show');
+        //         menuBtn.classList.add('active');
+        //     }
+        // };
 
         // document.querySelector('body').addEventListener('click', function() {
         //     nav.classList.remove('nav-show');
