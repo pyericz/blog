2fbe7ca7b7966dc7a6ea7f1f4e9d6f862d058298
Eric Zhang <eric.zhang@avatarworks.com>
2017-09-08 18:07:23 +0800

    update scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,43 +1,45 @@
 document.addEventListener("DOMContentLoaded",function(){
-    var h=function(a){
-        var href = a.getAttribute("href");
+    var scroll=function(element){
+        var href = element.getAttribute("href");
         var index = href.indexOf('#');
         var tag = href.substr(index);
         var top = $(tag).offset().top - 62;
         $("html, body").animate({ scrollTop: top }, {duration: 300, easing: "easeOutSine"});
 
-    },f=document.querySelectorAll("a.scroll");
+    },elements=document.querySelectorAll("a.scroll");
 
     var uri = document.baseURI;
-    (function b(c,d){
-        var e=c.item(d);
-        var href = e.getAttribute("href");
-        if (href && uri.indexOf(href) != -1) {
-            h(e);
-        }
-        e.addEventListener("click",function(b){
-            var href0 = uri;
-            var href1 = b.currentTarget.href;
 
-            var index0 = href0.indexOf('#');
-            var index1 = href1.indexOf('#');
+    (function parse(index){
+
+        var element=elements.item(index);
+        var href = element.getAttribute("href");
+
+        if (href && uri.indexOf(href) != -1) {
+            scroll(element);
+        }
+        element.addEventListener("click",function(event){
+            var from = uri;
+            var to = event.currentTarget.href;
+
+            var index0 = from.indexOf('#');
+            var index1 = to.indexOf('#');
 
             if (index0 != -1) {
-                href0 = href1.substr(0, index0);
+                from = from.substr(0, index0);
             }
 
             if (index1 != -1) {
-                href1 = href1.substr(0, index1);
+                to = to.substr(0, index1);
             }
 
-            if (href0 == href1) {
-                b.preventDefault();
-                h(e);
+            if (from == to) {
+                event.preventDefault();
+                scroll(element);
             }
         });
-        if(d)
-            return b(c,d-1);
-        else
-            return 0;
-    })(f,f.length-1);
+
+        if(index>0) parse(index-1);
+
+    })(elements.length-1);
 });
