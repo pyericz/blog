9a6f2013dd4aabfe2891c5654025872cf81ba0fe
Eric Zhang <eric.zhang@avatarworks.com>
2017-09-13 21:22:02 +0800

    post bezier-curve

---
+++ _posts/2017-09-07-bezier-curve.md
@@ -0,0 +1,115 @@
+---
+layout: post
+title: 贝塞尔曲线
+date: 2017-09-07
+categories: math
+tag: [bezier, curve, bernstein, polynomial]
+mathjax: true
+---
+* content
+{:toc}
+
+我们在上一篇文章《[伯恩斯坦多项式](/math/2017/09/06/bernstein-polynomial/)》中，我们提到了伯恩斯坦多项式的一般形式
+
+$$
+B_n(t) = \sum_{i=0}^{n}\beta_i \cdot b_{i, n}(t)
+$$
+
+其中，
+
+$$
+b_{i,n}(t) = \binom{n}{i}\cdot t^{i} \cdot (1-t)^{(n-i)}, \quad t\in[0, 1]
+$$
+
+是n阶伯恩斯坦基底多项式。而$$\beta_i$$叫做伯恩斯坦系数。当伯恩斯坦系数是二维平面中的一系列固定点时，伯恩斯坦多项式就演变成了本篇要讨论的贝塞尔曲线(Bézier curve)。
+
+
+
+<style>
+
+.curve, .line {
+  fill: none;
+  stroke-width: 1px;
+}
+.curve {
+  stroke: red;
+  stroke-width: 3px;
+}
+.control {
+  fill: #ccc;
+  stroke: #000;
+  stroke-width: .5px;
+	cursor: move;
+}
+.control.drag, .control:hover {
+	fill: #fe0;
+}
+.controltext {
+  font-size: .6em;
+}
+svg {
+  display: inline-block;
+}
+.vis {
+    <!-- border: 1px solid #ddd; -->
+}
+</style>
+
+
+<script src="/assets/js/2017/09/07/d3.min.js"></script>
+<script src="/assets/js/2017/09/07/control.js"></script>
+
+
+## 推导
+
+结合伯恩斯坦多项式的前几阶展开式，我们可以得到相应的贝塞尔曲线形式。
+
+### 线性曲线
+对二阶伯恩斯坦多项式展开，得到如下形式：
+
+$$
+B(t) = P_0 \cdot (1-t) + P_1 \cdot t  = P_0 + (P_1 - P_0) \cdot t
+$$
+
+从公式可以看出，二阶展开式对应的是介于$$P_0$$和$$P_1$$之间的线性插值点。
+
+<div id="vis0" class="vis">
+	<script>
+		start(2, "#vis0");
+	</script>
+</div>
+
+
+### 二次方曲线
+
+<div id="vis1" class="vis">
+	<script>
+		start(3, "#vis1");
+	</script>
+</div>
+
+
+### 三次方曲线
+
+<div id="vis2" class="vis">
+	<script>
+		start(4, "#vis2");
+	</script>
+</div>
+
+### 四次方曲线
+
+<div id="vis3" class="vis">
+	<script>
+		start(5, "#vis3");
+	</script>
+</div>
+
+
+
+
+
+
+## 参考
+
+- [Animated Bézier Curves](https://www.jasondavies.com/animated-bezier/)
---
+++ assets/js/2017/09/07/control.js
@@ -0,0 +1,157 @@
+function start(n, tag) {
+    var container = document.querySelector(tag);
+    var w = container.offsetWidth,
+        h = 400,
+        t = .5,
+        delta = .01,
+        padding = 10,
+        bezier = {},
+        stroke = d3.scale.category20b(),
+	    line = d3.svg.line().x(x).y(y),
+        orders = d3.range(n, n + 1);
+
+    var offset = w / 2-100;
+    var points = [
+        {x: 0 + offset, y: 346},
+        {x: 160 + offset, y: 169},
+        {x: 84 + offset,y: 42},
+        {x: 0 + offset, y: 94},
+        {x: 47 + offset, y: 160}
+    ];
+
+
+    var vis = d3.select(tag).selectAll("svg")
+        .data(orders)
+        .enter().append("svg")
+        .attr("width", w + 2 * padding)
+        .attr("height", h + 2 * padding)
+        .append("g")
+        .attr("transform", "translate(" + padding + "," + padding + ")");
+
+
+    update();
+
+    vis.selectAll("circle.control")
+        .data(function(d) { return points.slice(0, d); })
+        .enter().append("circle")
+        .attr("class", "control")
+        .attr("r", 7)
+        .attr("cx", x)
+        .attr("cy", y)
+        .call(d3.behavior.drag()
+              .on("dragstart", function(d) {
+                  this.__origin__ = [d.x, d.y];
+				  d3.select(this).classed("drag", true);
+              })
+              .on("drag", function(d) {
+                  d.x = Math.min(w-20, Math.max(0, this.__origin__[0] += d3.event.dx));
+                  d.y = Math.min(h-20, Math.max(0, this.__origin__[1] += d3.event.dy));
+                  bezier = {};
+                  update();
+                  vis.selectAll("circle.control")
+                      .attr("cx", x)
+                      .attr("cy", y);
+              })
+              .on("dragend", function() {
+                  delete this.__origin__;
+				  d3.select(this).classed("drag", false);
+              }));
+
+    vis.append("text")
+        .attr("class", "t")
+        .attr("x", w / 2)
+        .attr("y", h)
+        .attr("text-anchor", "middle");
+
+    vis.selectAll("text.controltext")
+        .data(function(d) { return points.slice(0, d); })
+        .enter().append("text")
+        .attr("class", "controltext")
+        .attr("dx", "10px")
+        .attr("dy", ".4em")
+        .text(function(d, i) { return "P" + i; });
+
+
+var last = 0;
+d3.timer(function(elapsed) {
+  t = (t + (elapsed - last) / 5000) % 1;
+  last = elapsed;
+  update();
+});
+
+function update() {
+  var interpolation = vis.selectAll("g")
+      .data(function(d) { return getLevels(d, t); });
+  interpolation.enter().append("g")
+      .style("fill", colour)
+      .style("stroke", colour);
+
+  var circle = interpolation.selectAll("circle")
+      .data(Object);
+  circle.enter().append("circle")
+      .attr("r", 4);
+  circle
+      .attr("cx", x)
+      .attr("cy", y);
+
+  var path = interpolation.selectAll("path")
+      .data(function(d) { return [d]; });
+  path.enter().append("path")
+      .attr("class", "line")
+      .attr("d", line);
+  path.attr("d", line);
+
+  var curve = vis.selectAll("path.curve")
+      .data(getCurve);
+  curve.enter().append("path")
+      .attr("class", "curve");
+  curve.attr("d", line);
+
+  vis.selectAll("text.controltext")
+      .attr("x", x)
+      .attr("y", y);
+  vis.selectAll("text.t")
+      .text("t=" + t.toFixed(2));
+}
+
+function interpolate(d, p) {
+  if (arguments.length < 2) p = t;
+  var r = [];
+  for (var i=1; i<d.length; i++) {
+    var d0 = d[i-1], d1 = d[i];
+    r.push({x: d0.x + (d1.x - d0.x) * p, y: d0.y + (d1.y - d0.y) * p});
+  }
+  return r;
+}
+
+function getLevels(d, t_) {
+  if (arguments.length < 2) t_ = t;
+  var x = [points.slice(0, d)];
+  for (var i=1; i<d; i++) {
+    x.push(interpolate(x[x.length-1], t_));
+  }
+  return x;
+}
+
+function getCurve(d) {
+  var curve = bezier[d];
+  if (!curve) {
+    curve = bezier[d] = [];
+    for (var t_=0; t_<=1; t_+=delta) {
+      var x = getLevels(d, t_);
+      curve.push(x[x.length-1][0]);
+    }
+  }
+  return [curve.slice(0, t / delta + 1)];
+}
+
+function colour(d, i) {
+  stroke(-i);
+  return d.length > 1 ? stroke(i) : "red";
+}
+
+function x(d) { return d.x; }
+function y(d) { return d.y; }
+
+}
+
